c5693f602ea7735adddbd7465354ee1986bc5cb7
ravindra-agnihotri01 <ravindra.swt@gmail.com>
2021-10-02 14:36:41 +0530

    new project added

---
+++ src/main/java/accessModifiers/PadosiKaGhar.java
@@ -0,0 +1,9 @@
+package accessModifiers;
+
+public class PadosiKaGhar {
+
+    public static void main(String[] args) {
+        AccessModifiers.chotu;
+        AccessModifiers.cycle
+    }
+}
---
+++ src/test/java/ClassCall.java
@@ -0,0 +1,10 @@
+import accessModifiers.AccessModifiers;
+
+public class ClassCall extends AccessModifiers{
+    public static void createMethod(){
+        AccessModifiers.tijori;
+        AccessModifiers.chotu;
+        AccessModifiers.cycle;
+    }
+
+}
